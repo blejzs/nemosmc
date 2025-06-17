3d5d9cfa6fb4cddd08df77add431f68554bb9e64
blejzs <weewior@gmail.com>
2025-06-17 05:09:35 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta charset="UTF-8" />
+<title>Regulamin</title>
+</head>
+<body style="margin:0; padding:0;">
+<embed src="TwojRegulamin.pdf" type="application/pdf" width="100%" height="100vh" />
+</body>
+</html>
